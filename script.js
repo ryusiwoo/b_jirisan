2495c729e01f93cfe616f29bd72ffaaff9d1224b
ryusiwoo <colkol0516@gmail.com>
2025-04-15 15:34:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBPHER9uMFWodYGEVsdj2KGY_m8HEOCUAQ",
-    authDomain: "comments-24767.firebaseapp.com",
-    databaseURL: "https://comments-24767-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "comments-24767",
-    storageBucket: "comments-24767.firebasestorage.app",
-    messagingSenderId: "559876960346",
-    appId: "1:559876960346:web:faf067629f4c00497de863",
-    measurementId: "G-T2DTS2SY4D"
+    apiKey: "AIzaSyAejvBpHRHrOoLUbCaSHWl3_GvXQ1k10kQ",
+    authDomain: "jirisan-8a0a9.firebaseapp.com",
+    databaseURL: "https://jirisan-8a0a9-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "jirisan-8a0a9",
+    storageBucket: "jirisan-8a0a9.firebasestorage.app",
+    messagingSenderId: "623824081076",
+    appId: "1:623824081076:web:68a519252583e490aa87e1",
+    measurementId: "G-7MHTPJZ8H5"
   };
 
 
